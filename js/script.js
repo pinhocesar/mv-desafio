c554c9ed701d1fc181f5a08308b55f079b3fedec
pinhocesar <caesaraugustus1806@gmail.com>
2021-10-22 06:31:15 -0300

    funcionalidades implementadas, fase de ajuste

--- js/script.js
+++ js/script.js
@@ -15,4 +15,53 @@ $(document).ready(function(){
     }); 
   });
 
-//Configura criação da lista
+//Valida preenchimento e chama função de adicionar item (falta colocar validação de itens repetidos)
+function validarItem(nomeCol, cpfCol, itemCes){
+    let nome = document.getElementById(nomeCol).value;
+    let cpf = document.getElementById(cpfCol).value;
+    let item = document.getElementById(itemCes).value;
+
+    if (nome == "")
+        alert("Favor adicionar seu nome.");
+    else if (cpf == "")
+        alert("Favor adicionar número do CPF.");
+    else if (item == "0")
+        alert("Favor selecionar um dos itens da lista.");
+    else adicionarItem(nome, parseInt(cpf), item);
+}
+
+//Adiciona dados em um item da cesta
+function adicionarItem(nomeItm, cpfItm, itemItm){
+    let novoItem = {nome:nomeItm, cpf:cpfItm, item:itemItm};
+
+    if (typeof(Storage) !== "undefined"){
+        let cesta = localStorage.getItem("cesta");
+        if (cesta == null) cesta = [];
+        else cesta = JSON.parse(cesta);
+        cesta.push(novoItem);
+        localStorage.setItem("cesta",JSON.stringify(cesta))
+        alert("Item adicionado na cestinha :)");
+        location.reload();
+    } 
+}
+
+//Lista itens na cesta
+function listarCesta(){
+    document.getElementById('itemList').innerHTML = "";
+    if (typeof(Storage) !== "undefined"){
+        let cesta = localStorage.getItem("cesta");
+        document.getElementById('itemList').innerHTML += "<h3>Conteúdo da cesta</h3>";
+        if (cesta == null)
+            document.getElementById('itemList').innerHTML += "<p>Ainda não há itens na cestinha :(</p>";
+        else{
+            usuarios = JSON.parse(cesta);
+            usuarios.forEach(nomeItm => {
+                document.getElementById('itemList').innerHTML += "<ul>";
+                document.getElementById('itemList').innerHTML += "<li>Nome do colaborador: "+nomeItm.nome+"</li>";
+                document.getElementById('itemList').innerHTML += "<li>CPF do colaborador: "+nomeItm.cpf+"</li>";
+                document.getElementById('itemList').innerHTML += "<li>Item selecionado: "+nomeItm.item+"</li>";
+                document.getElementById('itemList').innerHTML += "</ul>";
+            });
+        }
+    }    
+}
